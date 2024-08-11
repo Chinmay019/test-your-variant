e94ba8ca825cb2d2c7e65cf21a1d1084caf9b799
Chinmay019 <chinmaynaik019@gmail.com>
2024-08-11 11:55:21 +0530

    created navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,7 @@
+import { Navbar } from "@/components/Navbar/page";
+
 export default function Home () {
   return (
-    <div className="navbar bg-base-300">
-      <div className="navbar-start">
-        <a className="btn btn-ghost text-xl">Test Your Variant</a>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>All Cars</a></li>
-          <li>
-            <details>
-              <summary>Services</summary>
-              <ul className="p-2 menu xl:menu-horizontal bg-base-0 rounded-box lg:min-w-max">
-                <div >
-                  <li className="max-content"><a>List Your Car</a></li>
-                  <li><a>Contact Us</a></li>
-                </div>
-              </ul>
-            </details>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <div className="flex gap-2">
-          <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-          </div>
-          <div className="dropdown dropdown-end">
-            <div tabIndex={ 0 } role="button" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-              </div>
-            </div>
-            <ul
-              tabIndex={ 0 }
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Navbar />
   );
 }
