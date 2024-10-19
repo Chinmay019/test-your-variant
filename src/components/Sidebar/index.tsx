76d08ad71f51c848d714fe40efeda0f69ef50b32
Chinmay019 <chinmaynaik019@gmail.com>
2024-10-20 00:17:43 +0530

    sidebar changes

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,39 +7,42 @@ import FuelFilterComp from "../Filter/fuelFilter";
 const Sidebar = () => {
     return (
         <div className="h-[80vh] overflow-auto w-[375px] border-2 border-gray-300 md:rounded-md lg:rounded-lg m-8">
-            <h2 className="text-2xl font-semibold mx-10 mt-5">Filters</h2>
-            <div className="flex flex-col justify-around items-center">
-                <div className="mt-3">
-                    <label className="font-medium">
-                        Brands
-                    </label>
-                    <BrandFilterComp />
-                </div>
-                <div className="mt-3">
-                    <label className="font-medium">
-                        Location 📌
-                    </label>
-                    <LocationFilterComp />
-                </div>
-                <div className="mt-3">
-                    <label className="font-medium">
-                        Categories
-                    </label>
-                    <CategoryFilterComp />
-                </div>
-                <div className="mt-3">
-                    <label className="font-medium">
-                        Transmission Type
-                    </label>
-                    <TransmissionFilterComp />
-                </div>
-                <div className="my-3">
-                    <label className="font-medium">
-                        Fuel Type
-                    </label>
-                    <FuelFilterComp />
+            <div className="p-4">
+                <h2 className="text-2xl font-semibold mx-10 mt-5">Filters</h2>
+                <div className="flex flex-col justify-around items-center">
+                    <div className="mt-3">
+                        <label className="font-medium">
+                            Brands
+                        </label>
+                        <BrandFilterComp />
+                    </div>
+                    <div className="mt-3">
+                        <label className="font-medium">
+                            Location 📌
+                        </label>
+                        <LocationFilterComp />
+                    </div>
+                    <div className="mt-3">
+                        <label className="font-medium">
+                            Categories
+                        </label>
+                        <CategoryFilterComp />
+                    </div>
+                    <div className="mt-3">
+                        <label className="font-medium">
+                            Transmission Type
+                        </label>
+                        <TransmissionFilterComp />
+                    </div>
+                    <div className="my-3">
+                        <label className="font-medium">
+                            Fuel Type
+                        </label>
+                        <FuelFilterComp />
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
